66bf13f518cd4b6b2fb53392af53d7c533948d48
Dirk Eddelbuettel <edd@debian.org>
2020-10-20 09:36:33 -0500

    updated answer

--- 64428776/answer.cpp
+++ 64428776/answer.cpp
@@ -4,20 +4,16 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-int test(arma::sp_mat W){
-  Rcpp::Rcout << "W is good\n";
-  //W.print("W");
-  return 0;
+void spmatTest(arma::sp_mat M) {
+  // show a simple access of matrix,
+  // here we just sum elements on diagonal
+  Rcpp::Rcout << "Sum of diag is "
+              << arma::as_scalar(arma::sum(M.diag()))
+              << std::endl;
 }
 
 /*** R
-# define the diagonal matrix
-nrows <- 1e6
-W <- Matrix::Diagonal(nrows)
-
-# call Rcpp function
-test(W)
-
-# also:  test(Matrix::Diagonal(1e7))
-#        test(Matrix::Diagonal(1e8))
+spmatTest( Matrix::Diagonal( 1e6 ))
+spmatTest( Matrix::Diagonal( 1e7 ))
+spmatTest( Matrix::Diagonal( 1e8 ))
 */
