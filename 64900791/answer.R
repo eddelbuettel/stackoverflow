3fd50015fa349e008d0258faa891508c31bfbfaf
Dirk Eddelbuettel <edd@debian.org>
2020-11-18 14:56:17 -0600

    edits

--- 64900791/answer.R
+++ 64900791/answer.R
@@ -1,9 +1,8 @@
 library(DBI)
 suppressMessages(library(RPostgreSQL))
 drv <- dbDriver("PostgreSQL")
-conn <- dbConnect(drv, dbname="beancounter")
+conn <- dbConnect(drv, dbname="my_local_db_name_here")
 
-#conn <- dbConnect(drv = RPostgreSQL::PostgreSQL(), dbname = "my_db", host = "localhost")
 mydf <- data.frame(column_1 = c("A", "B", "C"), column_2 = c(1, Inf, 3))
 dbWriteTable(conn, "mytable", mydf, row.names = FALSE)
 from_df <- dbReadTable(conn, "mytable")
