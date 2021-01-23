7b47f8888818ff650bdea4a4f7aa45c84bc7c018
Dirk Eddelbuettel <edd@debian.org>
2021-01-23 08:29:23 -0600

    small edits

--- 65838609/answer.cpp
+++ 65838609/answer.cpp
@@ -1,26 +1,28 @@
 
-#define BOOST_NO_AUTO_PTR
-
 #include <Rcpp.h>
 #include <fstream>
+
+// we need this define under current Boost to silence warning about use of deprecated auto_ptr
+#define BOOST_NO_AUTO_PTR
 #include <boost/uuid/detail/md5.hpp>
 #include <boost/algorithm/hex.hpp>
 
+// make sure we 'see' header files from BH
 // [[Rcpp::depends(BH)]]
-
-using boost::uuids::detail::md5;
 
 // [[Rcpp::export]]
 bool hasher(const std::string& infilename, const std::string& outfilename) {
 
   // cf https://stackoverflow.com/questions/48545330/c-fastest-way-to-read-file-line-by-line
-  //    https://stackoverflow.com/questions/55070320/how-to-calculate-md5-of-a-file-using-boost
   std::ifstream infile(infilename);
   std::ofstream outfile(outfilename);
   std::string line;
+
   while (std::getline(infile, line)) {
     // line contains the current line
-    md5 hash;
+
+    // cf https://stackoverflow.com/questions/55070320/how-to-calculate-md5-of-a-file-using-boost
+    boost::uuids::detail::md5 hash;
     md5::digest_type digest;
     hash.process_bytes(line.data(), line.size());
     hash.get_digest(digest);
