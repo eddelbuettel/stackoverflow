3828503d5acc79f0a7e293e3f65d8e862d0297c5
Dirk Eddelbuettel <edd@debian.org>
2021-03-20 12:07:48 -0500

    updated answer

--- 66720922/answer.cpp
+++ 66720922/answer.cpp
@@ -8,7 +8,14 @@ Rcpp::NumericMatrix vec2mat(Rcpp::NumericVector vec, int n, int k) {
     return mat;
 }
 
+// [[Rcpp::export]]
+Rcpp::NumericMatrix vec2mat2(Rcpp::NumericVector s, int n, int k) {
+    Rcpp::NumericMatrix mat(n, k, s.begin());
+    return mat;
+}
+
 /*** R
 v <- (1:9) * 1.0  # numeric
 vec2mat(v, 3, 3)
+vec2mat2(v, 3, 3)
 */
