9e75540078eba12a2c0d52d317b65670240a9702
Dirk Eddelbuettel <edd@debian.org>
2021-11-09 11:13:15 -0600

    new answer (and one mod)

--- 69786731/answer.R
+++ 69786731/answer.R
@@ -7,3 +7,6 @@ DF <- data.frame(X1=Vec[-1], X2=Vec[-length(Vec)])
 coef(lm(X1 ~ X2, DF))[2]
 
 cov(DF$X1, DF$X2) / var(DF$X2)
+
+myN <- nrow(DF)
+(sum(DF[,1]*DF[,2])*myN - sum(DF[,1])*sum(DF[,2])) / (myN * sum(DF[,1]^2) - (sum(DF[,1])^2))
