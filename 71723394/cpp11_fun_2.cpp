8b9344a495e48b23290467d06d08c9a5903075ed
Dirk Eddelbuettel <edd@debian.org>
2022-04-08 13:05:27 -0500

    answer for cpp11 (the CRAN package, not the standard)

---
+++ 71723394/cpp11_fun_2.cpp
@@ -0,0 +1,25 @@
+#include <cpp11.hpp>
+#include <math.h>
+
+using namespace cpp11;
+using namespace std;
+
+[[cpp11::register]]
+int cpp11_fun(int Q, integers nbCluster) {
+    int q;
+    int sum_cases=0;
+    writable::integers start(Q), end(Q);
+
+    for(q=0 ; q<Q ; q++){
+        // the total number of clusters (eg if man/woman and 10 countries: total of 12 cases)
+        sum_cases += nbCluster[q];
+        if (q == 0){
+            start[q] = 0;
+            end[q] = nbCluster[q];
+        } else {
+            start[q] = start[q-1] + nbCluster[q-1];
+            end[q] = end[q-1] + nbCluster[q];
+        }
+    }
+    return(q);
+}
---
+++ 71723394/question.R
@@ -0,0 +1,5 @@
+setwd("~/git/stackoverflow/71723394")
+
+library(cpp11)
+
+cpp_source("cpp11_fun.cpp")
