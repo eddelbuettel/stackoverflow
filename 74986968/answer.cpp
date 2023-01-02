3bd416c3ae73d31206f1b54ccda97f0e7810c7e0
Dirk Eddelbuettel <edd@debian.org>
2023-01-02 16:27:14 -0600

    Updated answer

--- 74986968/answer.cpp
+++ 74986968/answer.cpp
@@ -3,6 +3,8 @@
 #include <fmt/chrono.h>
 
 int main() {
-    fmt::print("{:%S}\n", std::chrono::system_clock::now().time_since_epoch());
+    auto now = std::chrono::system_clock::now();
+    auto sse = now.time_since_epoch();
+    fmt::print("{:%FT%H:%M:}{:%S}\n", now, sse);
     exit(0);
 }
