246364c46b871e471ab1e9e3d86ef52669cfcc52
Dirk Eddelbuettel <edd@debian.org>
2023-11-07 14:30:49 -0600

    New question

---
+++ 77441228/question.cpp
@@ -0,0 +1,24 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List test_bug(int N) {
+
+    std::vector<int>    inds;
+    std::vector<double> vals;
+
+    for (int k = 0; k < N; k++) {
+        int i_k = k;
+        double x_k = k;
+        inds.push_back(i_k);
+        vals.push_back(x_k);
+    }
+
+    return List::create(wrap(inds), wrap(vals));
+}
+
+/*** R
+for (k in 1:100) {
+    test <- test_bug(100000)
+}
+*/
